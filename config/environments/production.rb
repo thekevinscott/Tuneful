b58845f126ddb19ea22ae694a676eec891ecd3f2
Kevin Scott <thekevinscott@gmail.com>
2010-01-24 13:12:41 -0600

    Production shows errors now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,11 @@ config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 
+# AND NOW THEYRE NOT BWAHAHA
+config.action_controller.consider_all_requests_local = true
+config.action_view.debug_rjs                         = true
+config.action_controller.perform_caching             = false
+
 # See everything in the log (default is :info)
  config.log_level = :debug
 
