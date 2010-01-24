d77b22bfaf62e8a97ae48dc0d6bc7b64de4e2b40
Kevin Scott <thekevinscott@gmail.com>
2010-01-24 12:48:28 -0600

    More gem issues

--- config/environment.rb
+++ config/environment.rb
@@ -23,8 +23,6 @@ Rails::Initializer.run do |config|
   #config.gem gemcutter
   #config.gem mongo
   #config.gem mongo_ext
-  config.gem "mongo_mapper"
-  config.gem "youtube-g"  
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
