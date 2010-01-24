a05d795f9c93c5fd2626514c220c2a69c8739e56
Kevin Scott <thekevinscott@gmail.com>
2010-01-24 12:52:57 -0600

    trying for mongo

--- config/initializers/mongo_config.rb
+++ config/initializers/mongo_config.rb
@@ -1,1 +1,6 @@
+MongoMapper.connection = Mongo::Connection.new('db.mongohq.com', 
+27017, :auto_reconnect => true) 
+
 MongoMapper.database = "tuneful_#{Rails.env}"
+
+MongoMapper.database.authenticate('thekevinscott', 'tuneful1Fp3v')
